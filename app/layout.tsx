287116593ac8f29c371fb6c653324923c966945c
mdeport <mateo.deport@ecole-isitech.fr>
2025-11-04 12:42:47 +0100

    add sidebar and dashboard admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/features/layout/header";
+import ConditionalHeader from "@/components/features/layout/ConditionalHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-screen bg-primary-foreground`}
       >
-        <Header />
+        <ConditionalHeader />
         <main>{children}</main>
       </body>
     </html>
